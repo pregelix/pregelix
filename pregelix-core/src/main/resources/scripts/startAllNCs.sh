f3777d13d53ac5a17a252811770948236e8a4a10
buyingyi <buyingyi@123451ca-8445-de46-9d55-352943316053>
2013-03-24 07:20:08 +0000

    cross merge fullstack_release_candidate into trunk
    git-svn-id: https://hyracks.googlecode.com/svn/trunk/fullstack@3208 123451ca-8445-de46-9d55-352943316053

--- pregelix-core/src/main/resources/scripts/startAllNCs.sh
+++ pregelix-core/src/main/resources/scripts/startAllNCs.sh
@@ -2,5 +2,5 @@ PREGELIX_PATH=`pwd`
 
 for i in `cat conf/slaves`
 do
-   ssh $i "cd ${PREGELIX_PATH}; bin/startnc.sh"
+   ssh $i "cd ${PREGELIX_PATH}; export JAVA_HOME=${JAVA_HOME}; bin/startnc.sh"
 done
